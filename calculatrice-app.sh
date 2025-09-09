df2bfcfa41c1cfb7f74012b3ba5a3101e48feac5
bbauer02 <bbauer02@gmail.com>
2025-09-09 10:24:53 +0200

    modification du script BASH pour construire l'application dans le Build

--- calculatrice-app.sh
+++ calculatrice-app.sh
@@ -1,4 +1,7 @@
 #!/bin/bash 
 
-echo "Hello Jenkins from Github!"
+echo "********************************"
+echo "CONSTRUCTION DE L'APPLICATION SUR LE PORT 3000"
+docker-compose up -d --build
+echo "********************************"
 date
