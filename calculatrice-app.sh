161f9705af1adbe76ac32e5cda6bb9b23ef17ee3
bbauer02 <bbauer02@gmail.com>
2025-09-09 10:31:19 +0200

    modification du script BASH pour construire l'application dans le Build

--- calculatrice-app.sh
+++ calculatrice-app.sh
@@ -2,6 +2,6 @@
 
 echo "********************************"
 echo "CONSTRUCTION DE L'APPLICATION SUR LE PORT 3000"
-docker compose up -p calculatriceapp -d --build
+docker compose up -d --build
 echo "********************************"
 date
